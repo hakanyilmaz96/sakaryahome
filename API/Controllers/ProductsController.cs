f7038d357c4f28bd7f42481483cf12bb954c5a9b
Hakan Yılmaz <hyilmazdev@gmail.com>
2023-11-19 02:56:17 +0300

    Revert "End of section 3"
    This reverts commit 0a5deda87f2fe2604edcd5a12b3be2ae1cec3571.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,5 @@
 using System.Drawing;
 using Core.Entities;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,36 +12,24 @@ namespace API.Controllers
     public class ProductsController : ControllerBase
     {
        
-    
-        private readonly IProductRepository _repo;
-        public ProductsController(IProductRepository repo)
+        public StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            _repo = repo;
+            _context = context;
+            
         }
         
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _repo.GetProductsAsync();
-            return Ok(products);
+            var products = await _context.Products.ToListAsync();
+            return products;
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _repo.GetProductByIdAsync(id);
-        }
-
-        [HttpGet("brands")]
-        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
-        {
-            return Ok(await _repo.GetProductBrandsAsync());
-        }
-
-        [HttpGet("types")]
-        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
-        {
-            return Ok(await _repo.GetProductTypesAsync());
+            return await _context.Products.FindAsync(id);
         }
 
     }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,3 @@
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,8 +16,6 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 }
 );
 
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,19 +28,5 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-var context = services.GetRequiredService<StoreContext>();
-var logger = services.GetRequiredService<ILogger<Program>>();
-try
-{
-    await context.Database.MigrateAsync();
-    await StoreContextSeed.SeedAsync(context);
-}
-catch (Exception ex)
-{
-    logger.LogError(ex, "An error occured during migration");
-}
 
 app.Run();
