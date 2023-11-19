8688b3baa8b93c32805fe2b9f1a7b2cc61b87663
Hakan Yılmaz <hyilmazdev@gmail.com>
2023-11-19 03:13:08 +0300

    End of section 3

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+
+
+
+
+        
+    }
+}
