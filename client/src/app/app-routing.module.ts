e2912328487e05fe057d2141dc1f844aeefd7641
Hakan Yilmaz <hyilmazdev@gmail.com>
2023-12-07 23:00:17 +0300

    Client-side basket created.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,8 +10,9 @@ const routes: Routes = [
   {path: 'test-error',component: TestErrorComponent},
   {path: 'not-found',component: NotFoundComponent},
   {path: 'server-error',component: ServerErrorComponent},
-
   {path: 'shop',loadChildren: () => import("./shop/shop.module").then(m => m.ShopModule)},
+  {path: 'basket',loadChildren: () => import("./basket/basket.module").then(m => m.BasketModule)},
+  {path: 'checkout',loadChildren: () => import("./checkout/checkout.module").then(m => m.CheckoutModule)},
   {path: '**',redirectTo:'', pathMatch: 'full'},
 
 ];
