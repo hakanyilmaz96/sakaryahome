7675f19824e0c26c91e3760626aac92f5978f9a2
Hakan Yilmaz <hyilmazdev@gmail.com>
2023-12-09 17:40:34 +0300

    Setting API validation

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -11,8 +13,10 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper _mapper;
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
@@ -24,9 +28,10 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customerBasket = _mapper.Map<CustomerBasket>(basket);
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
             return Ok(updatedBasket);
         }
 
--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -4,17 +4,17 @@ namespace API.Dtos
 {
     public class AddressDto
     {
-       
+        [Required]
         public string FirstName { get; set; }
-
+        [Required]
         public string LastName { get; set; }
-
+        [Required]
         public string Street { get; set; }
-
+        [Required]
         public string City { get; set; }
-
+        [Required]
         public string State { get; set; }
-
+        [Required]
         public string Zipcode { get; set; }
     }
 }
