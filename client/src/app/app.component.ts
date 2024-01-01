4caaf5e84655b5340b226c6047f6a2cee04b53a7
Hakan Yilmaz <hyilmazdev@gmail.com>
2024-01-01 20:49:10 +0300

    Json files updated

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AccountService } from './account/account.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Skinet';
+  title = 'SakaryaHome';
   
   constructor(private basketService: BasketService, private accountService: AccountService){}
   
